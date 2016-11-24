db508010a8d1a26984a9bd453f8e51cba0d714b1
Jerome de Tychey <jdetychey@gmail.com>
2016-11-24 17:06:37 +0100

    working version <:- ))
    working version <:- ))

--- contracts/InsurancePoolDemo.sol
+++ contracts/InsurancePoolDemo.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.4.4;
 contract InsurancePool {
     /* stuff and arrays */
     address[] public newContracts;
-    uint public fee;
+    uint256 public fee;
     address public admin;
-    
+
     /* admin exclusivity */
+  
     modifier onlyAdmin {
         if(msg.sender != admin) throw;
         _;
@@ -15,61 +16,69 @@ contract InsurancePool {
     /* CONSTRUCTOR */
     // InsurancePool has to come with a supply so you have to send ether 
     //  at deployment ; fee is set a 0 by default
+
     function InsurancePool() {
         admin = msg.sender;
         fee = 0;
     }
 
     /* ADMIN METHODS */
+    
     function setFee(uint _fee) onlyAdmin {
         fee = _fee;
     }
-
+    
     function changeAdmin(address _admin) onlyAdmin {
         admin = _admin;
     }
-
+    
+    function load() payable {}
+    
     /* POLICY METHOD */
 
-    function createNewPolicy(string _url,
-                             uint _coverage,
-                             string _description) {
-        address _insuree = msg.sender;
-        uint _total = (msg.value - fee) + _coverage;
-        address newContract = new InsurancePolicy(_insuree, _url, _description, admin);
-        if (!newContract.send(_total)) //if smth happens when sending the tx is thrown
-        throw;
-        newContracts.push(newContract);
-    }
-    
+   function createNewPolicy(string _url,
+                            uint256 _coverage,
+                            string _description) payable {
+       uint256 _total = msg.value - fee + _coverage;
+       address _insuree = msg.sender;
+       address newContract = (new InsurancePolicy).value(_total)(_insuree,
+       _url, _description, admin);
+           newContracts.push(newContract);
+   }
+
+
     /* BLOCKCHAIN CLEARING */
+
       function kill() onlyAdmin {
         selfdestruct(admin);
     }
 }
 
 contract InsurancePolicy {
+    
+    InsurancePolicy ispayable;
     // for the sake of the demo we introduce Resolver a trusted third party
     /* stuff and arrays */
+    
     uint public balance;
     address public insuree;
     address public InsurancePool;
     address public resolver; //the address that will remotly activate the payout
     string public description;
     string public url;
-
+    
     /* resolver (admin) exclusivity */
 
     modifier onlyResolver {
         if(msg.sender != resolver) throw;
         _;
     }
-
+    
     /* constructor */
 
     function InsurancePolicy(address _insuree,
                     string _url,
-                    string _description, address _resolver) {
+                    string _description, address _resolver) payable {
         resolver = _resolver ; //the resolver is the admin of the InsurancePool
         insuree = _insuree;
         url = _url;
@@ -79,12 +88,11 @@ contract InsurancePolicy {
 
     /* resolver methods */
 
-    function payout() private onlyResolver() {
+    function payout() onlyResolver() {
             selfdestruct(insuree);
     }
 
-    function cancel() private onlyResolver() {
+    function cancel() onlyResolver() {
         selfdestruct(InsurancePool);
     }
-
 }
